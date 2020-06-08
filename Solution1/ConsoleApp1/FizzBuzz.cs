b215b8b8b6947ec87b8fd7b87ef40773399684f0
SergioFerreira <sergio.ferreira@ctw.bmwgroup.com>
2020-06-08 16:27:55 +0100

    implemented fizzbuzz

--- Solution1/ConsoleApp1/FizzBuzz.cs
+++ Solution1/ConsoleApp1/FizzBuzz.cs
@@ -1,12 +1,23 @@
 using System;
+using System.Text;
 
 namespace ConsoleApp1
 {
+    // Write a program that prints the numbers from 1 to 100.
+    // But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+    // For numbers which are multiples of both three and five print “FizzBuzz”."
     public static class FizzBuzz
     {
         public static string Execute(int value)
         {
-            throw new NotImplementedException();
+            var result = new StringBuilder();
+
+            if (value % 3 == 0) result.Append("Fizz");
+            if (value % 5 == 0) result.Append("Buzz");
+
+            return result.Length == 0
+                ? value.ToString()
+                : result.ToString();
         }
     }
 }
--- Solution1/TestProject1/UnitTest1.cs
+++ Solution1/TestProject1/UnitTest1.cs
@@ -1,3 +1,4 @@
+using ConsoleApp1;
 using NUnit.Framework;
 
 namespace TestProject1
@@ -5,9 +6,34 @@ namespace TestProject1
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void Returns_number_when_not_multiple_of_3_or_5([Values(1, 2, 4, 7, 8, 11)]int value)
         {
-            Assert.Pass();
+            var result = FizzBuzz.Execute(value);
+
+            Assert.AreEqual(value.ToString(), result);
+        }
+
+        [Test]
+        public void Returns_fizz_for_multiple_of_3([Values(3, 6, 9, 12)]int value)
+        {
+            var result = FizzBuzz.Execute(value);
+
+            Assert.AreEqual("Fizz", result);
+        }
+
+        [Test]
+        public void Returns_buzz_for_multiple_of_5([Values(5, 10, 20)]int value)
+        {
+            var result = FizzBuzz.Execute(value);
+
+            Assert.AreEqual("Buzz", result);
+        }
+        [Test]
+        public void Returns_fizzbuzz_for_multiple_of_3_and_5([Values(15, 30)]int value)
+        {
+            var result = FizzBuzz.Execute(value);
+
+            Assert.AreEqual("FizzBuzz", result);
         }
     }
 }
