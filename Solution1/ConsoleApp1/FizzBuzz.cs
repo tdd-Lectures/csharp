6ae3e00238b7bad0b3d55a0d8b238bafddbd81b3
Sergio Ferreira <sergio.ferreira@ctw.bmwgroup.com>
2020-06-09 12:58:24 +0100

    Update FizzBuzz.cs

--- Solution1/ConsoleApp1/FizzBuzz.cs
+++ Solution1/ConsoleApp1/FizzBuzz.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace ConsoleApp1
 {
+    // Write a program that prints the numbers from 1 to 100.
+    // But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+    // For numbers which are multiples of both three and five print “FizzBuzz”."
     public static class FizzBuzz
     {
         public static string Execute(int value)
